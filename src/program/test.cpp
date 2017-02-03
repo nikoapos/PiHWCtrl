bf587ab31552705ab425cc2ddba41c01d8fc679a
Nikolaos Apostolakos <nikoapos@gmail.com>
2017-02-03 17:37:14 +0000

    Reorganize the files to introduce the PiHWCtrl/pigpio directory

--- src/program/test.cpp
+++ src/program/test.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-#include <PiHWCtrl/SmartPigpio.h>
+#include <PiHWCtrl/pigpio/SmartPigpio.h>
+
+#include<pigpio.h>
  
 int main(int argc, char *argv[]){
   
   PiHWCtrl::SmartPigpio a {};
+  
+  for (unsigned int i = 0; i < 32; ++i) {
+    std::cout << "GPIO " << i << " mode: " << gpioGetMode(i) << '\n';
+  }
   
   return 0;
 }
