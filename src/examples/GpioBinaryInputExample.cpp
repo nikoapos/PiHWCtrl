c9921cc4922e270c0b21472e0bdcf60e14400f06
Nikolaos Apostolakos <nikoapos@gmail.com>
2017-03-13 11:54:01 +0100

    Add observer example for the GpioBinaryInput

--- src/examples/GpioBinaryInputExample.cpp
+++ src/examples/GpioBinaryInputExample.cpp
@@ -36,21 +36,29 @@
  * -----------------------
  * Materials:
  *   - A switch
- *   - A 10k resistor (used as pull-up)
+ *   - A 10k resistor (used as pull-down)
  * 
  * Connections:
  *   - Connect the GPIO-21 pin to the one side of the switch
- *   - Connect a GND pin to the other side of the switch
+ *   - Connect a 3.3V pin to the other side of the switch
  *   - Connect the one side of the resistor to the GPIO-21
- *   - Connect the other side of the resistor to a 3V3 pin
+ *   - Connect the other side of the resistor to a GND pin
  * 
  * Execution:
- * Turn the switch off and run the example. Then turn the switch on and re-run
- * it. You should see terminal messages indicating the position of the switch.
+ * Run the example. A message will appear indicating if the switch is ON or OFF.
+ * The program will continue to run for 10 seconds. during this time try to turn
+ * on and off the switch. You should see messages indicating your actions.
  */
 
 #include <iostream> // for std::cout
+#include <thread> // for std::this_thread
+#include <chrono> // for std::chrono_literals
 #include <PiHWCtrl/gpio/GpioBinaryInput.h> // for GpioBinaryInput
+#include <PiHWCtrl/controls/StateChangeFilter.h> // For StateChangeFilter
+
+// We introduce the symbols from std::chrono_literals so we can write time
+// like 500ms (500 milliseconds)
+using namespace std::chrono_literals;
 
 int main() {
   
@@ -58,6 +66,10 @@ int main() {
   // implements the PiHWCtrl::BinaryInput interface, so it can be used with any
   // function expecting the interface.
   PiHWCtrl::GpioBinaryInput input {21};
+  
+  //
+  // Getting the state of the pin using method calls
+  //
   
   // For checking if the input pin is on or off you can use the isOn() and
   // isOff() methods, which return a boolean.
@@ -68,7 +80,45 @@ int main() {
     std::cout << "GPIO 21 input is OFF\n";
   }
   
-  // For more details (like the possible exceptions thrown) have a look at the
-  // API documentation of the class!
+  //
+  // Getting the state of the pin using events
+  //
+  
+  // The GpioBinaryInput class implements the Observable<bool> interface, which
+  // means you can add observers that will be notified for the state of the pin.
+  // The following class is an implementation of such an observer, which will
+  // print on the screen the message ON or OFF.
+  class ScreenPrinter : public PiHWCtrl::Observer<bool> {
+  public:
+    void event(const bool& value) override {
+      std::cout << (value ? "ON" : "OFF") << '\n';
+    }
+  };
+  
+  // Create an instance of the observer class, which will be used for printing
+  // the messages. You need to pass a shared pointer to the GpioBinaryInput, so
+  // the best way to create it is by using the make_shared() method.
+  auto screen_printer = std::make_shared<ScreenPrinter>();
+  
+  // The GpioBinaryInput will generate events with its state continuously. That
+  // means that if we add the screen_printer observer directly we will get
+  // repetitive messages on the screen. To solve this problem you can wrap the
+  // observer with the StateChangeFilter decorator. This decorator will filter
+  // out any events for which the state is not changed and will forward only
+  // the events that the switch turns from OFF to ON and from ON to OFF.
+  auto wrapped_observer = std::make_shared<PiHWCtrl::StateChangeFilter<bool>>(screen_printer);
+  
+  // Add the observer to the input, so it will be notified for its events
+  input.addObserver(wrapped_observer);
+  
+  // To have the GpioBinaryInput generating events you have to start it by
+  // calling the start() method. This method gets a single argument, which is
+  // the frequency (in milliseconds) in which the events will be created. Here
+  // we want to get an event every 10 ms.
+  input.start(10);
+  
+  // Now we can sleep for 10 seconds before allowing the program to exit, so we
+  // can turn the switch on and off and see the messages on the screen.
+  std::this_thread::sleep_for(10s);
   
 }
