4fe5aa05c7eb75e50dcccf3a8416277bb4ada425
Nikolaos Apostolakos <nikoapos@gmail.com>
2017-03-21 15:27:51 +0100

    Make the HCSR04 implement the Observable and AnalogInput interfaces

--- include/PiHWCtrl/modules/HCSR04.h
+++ include/PiHWCtrl/modules/HCSR04.h
@@ -24,27 +24,108 @@
 #define PIHWCTRL_HCSR04_H
 
 #include <memory>
+#include <mutex>
+#include <atomic>
 #include <PiHWCtrl/HWInterfaces/BinaryInput.h>
 #include <PiHWCtrl/HWInterfaces/Switch.h>
+#include <PiHWCtrl/HWInterfaces/AnalogInput.h>
+#include <PiHWCtrl/HWInterfaces/Observable.h>
 
 namespace PiHWCtrl {
 
-class HCSR04 {
+/**
+ * @class HCSR04
+ * 
+ * @brief
+ * Class for controlling the HC-SR04 ultrasonic distance measurement sensor
+ * 
+ * @details
+ * The HC-SR04 measures distances by producing 8 ultrasound pulses at 40 kHz and
+ * measuring the time required for the pulses to be reflected by an object and
+ * come back. The way the HC-SR04 works is by using the two pins named Trig and
+ * Echo. A short pulse of 10us at the Trig pin starts a measurement. The HC-SR04
+ * sends the ultrasound pulses and waits for them to return. After the pulses
+ * return, it sets the Echo pin up for as much time as it took to the pulses to
+ * return. Note that if the pulses are not returned, the Echo pin will not be
+ * raised at all.
+ * 
+ * The HCSR04 class communicates with the device via a Switch for triggering the
+ * measurements and a BinaryInput for measuring the echo. Note that the class
+ * handles the case of no echo raised (no pulses returned), by returning the
+ * maximum distance. 
+ */
+class HCSR04 : public AnalogInput<float>, public Observable<float> {
   
 public:
   
+  /**
+   * @brief Creates an HCSR04 instance
+   * 
+   * @details
+   * In most of the cases you most probably want to use the GPIO pins for the
+   * trigger and echo parameters. If this is the case, you can use the classes
+   * in the PiHWCtrl/gpio package.
+   * 
+   * Any distance measured bigger than the parameter max_distance is going to
+   * be trimmed to this value. The default is set to 4 meters, which is the
+   * maximum range of the device. Note that this value will also be returned in
+   * the case the device does not detect any pulses coming back.
+   * 
+   * @param trigger
+   *    The object to receive the pulses to start the measurement
+   * @param echo
+   *    The object to read the echo delay from
+   * @param max_distance
+   *    The maximum distance to measure (in meters)
+   * @param sound_speed
+   *    The speed of sound to use (in meters/seconds)
+   */
   HCSR04(std::unique_ptr<Switch> trigger, std::unique_ptr<BinaryInput> echo,
-         float max_distance=4.);
+         float max_distance=4., float sound_speed=343);
 
-  virtual ~HCSR04() = default;
+  /// Destructor
+  virtual ~HCSR04();
   
+  /**
+   * @brief Measures the distance using the sensor
+   * 
+   * @details
+   * The time this method will block depends on the distance measured, as it
+   * depends on the time the ultrasound pulses need to travel back and forth.
+   * 
+   * @return The measured distance
+   */
   float readDistance();
+  
+  /// Redirects the call to the readDistance()
+  float readValue() override;
+  
+  /**
+   * @brief Enter continuous measurement mode
+   * 
+   * @details
+   * In this mode, the class will repeatedly use the sensor for measuring the
+   * distance and it will notify all the registered observers. The parameter
+   * is the idle time between performing two measurements and it can be used to
+   * guarantee that a consequent measurement is not affected by reflections of
+   * a previous one.
+   * 
+   * @param sleep_ms
+   *    The idle time between measurements (in milliseconds)
+   */
+  void start(unsigned int sleep_ms=60);
+  
+  /// Stop the continuous measurement mode
+  void stop();
   
 private:
   
   std::unique_ptr<Switch> m_trigger;
   std::unique_ptr<BinaryInput> m_echo;
   float m_max_dist;
+  float m_sound_speed;
+  mutable std::mutex m_mutex;
+  std::atomic<bool> m_observing {false};
   
 };
 
--- src/lib/modules/HCSR04.cpp
+++ src/lib/modules/HCSR04.cpp
@@ -24,6 +24,8 @@
 #include <thread> // for std::this_thread
 #include <algorithm> // for std::min
 #include <PiHWCtrl/modules/HCSR04.h>
+#include <PiHWCtrl/utils/EventGenerator.h>
+#include <PiHWCtrl/HWInterfaces/exceptions.h>
 
 // We introduce the symbols from std::chrono_literals so we can write time
 // like 500ms (500 milliseconds)
@@ -32,24 +34,33 @@ using namespace std::chrono_literals;
 namespace PiHWCtrl {
 
 HCSR04::HCSR04(std::unique_ptr<Switch> trigger, std::unique_ptr<BinaryInput> echo,
-               float max_distance)
-        : m_trigger(std::move(trigger)), m_echo(std::move(echo)), m_max_dist(max_distance) {
+               float max_distance, float sound_speed)
+        : m_trigger(std::move(trigger)), m_echo(std::move(echo)),
+          m_max_dist(max_distance), m_sound_speed(sound_speed) {
   m_trigger->turnOff();
 }
 
+HCSR04::~HCSR04() {
+  // Stop any threads generating events for this HCSR04
+  stop();
+}
+
 float HCSR04::readDistance() {
+  
+  std::lock_guard<std::mutex> lock {m_mutex};
+  
   // Set the trigger to on for 10us to start the measurement
   m_trigger->turnOn();
   std::this_thread::sleep_for(10us);
   m_trigger->turnOff();
   
-  // Wait for the measurement to happen. If this takes more than 100 milliseconds
-  // (equivalent to 17.24 meters, so way out of the range of the sensor) we return
-  // the maximum distance.
+  // Wait for the measurement to happen. It it takes more than what is required
+  // for the current maximum distance we return the maximum distance directly.
+  int max_time = 2E3 * m_max_dist / m_sound_speed;
   auto t1 = std::chrono::high_resolution_clock::now();
   for (; m_echo->isOff() ;) {
     auto t2 = std::chrono::high_resolution_clock::now();
-    if (t2 > t1 + 100ms) {
+    if (t2 > t1 + std::chrono::milliseconds(max_time)) {
       return m_max_dist;
     }
   }
@@ -59,15 +70,43 @@ float HCSR04::readDistance() {
   auto t2 = t1;
   for(; m_echo->isOn() ;) {
     t2 = std::chrono::high_resolution_clock::now();
-    if (t2 > t1 + 100ms) {
-      return m_max_dist;
-    }
   }
   auto time = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 
-  // Convert to meters
-  float dist = time / 5800.;
+  // Convert to meters : x(m) = (t(us) * sound_speed(m/s) / 2) / 10^6
+  float dist = time * m_sound_speed / 2e6;
+  // If the measured distance is greater than the maximum we return the maximum
   return std::min(dist, m_max_dist);
+}
+
+float HCSR04::readValue() {
+  return readDistance();
+}
+
+void HCSR04::start(unsigned int sleep_ms) {
+  if (m_observing) {
+    throw Exception() << "HCSR04 already started";
+  }
+  m_observing = true;
+  auto event_func = [this]() {
+    return readDistance();
+  };
+  auto notify_func = [this](float value) {
+    notifyObservers(value);
+  };
+  startEventGenerator<float>(event_func, notify_func, m_observing, sleep_ms);
+}
+
+void HCSR04::stop() {
+  if (m_observing) {
+    // This will trigger the EventGenerator thread to stop
+    m_observing = false;
+    // We have to wait until the thread signals that it stopped
+    while (!m_observing) {
+    }
+    // Now we can set again the flag to false
+    m_observing = false;
+  }
 }
 
 } // end of namespace PiHWCtrl
